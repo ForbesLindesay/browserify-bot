7566833428c89c66f85868d78987fbdf0a9fb6a8
Forbes Lindesay <forbes@lindesay.co.uk>
2014-02-28 15:10:40 +0000

    Improve things

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
 var barrage = require('barrage');
+var Promise = require('promise');
+var rimraf = Promise.denodeify(require('rimraf'));
 var npmUpdates = require('./lib/check-npm');
 var fetchNpm = require('./lib/fetch-npm');
 var build = require('./lib/build');
+var stat = Promise.denodeify(fs.stat);
 
 var next = npmUpdates();
 function pull() {
-  next().then(function (pkg) {
-    console.log(pkg.name + '@' + pkg['dist-tags'].latest);
-    if (true || !/^0/.test(pkg['dist-tags'].latest)) {
-      //console.dir(pkg);
-      return fetchNpm(pkg).then(function () {
-        return build(pkg);
-      });
-    }
-  }).done(function (res) {
+  next().then(processPackage).done(function (res) {
     pull();
   });
 }
 pull();
+
+function processPackage(pkg) {
+  var name = pkg.name;
+  var version = pkg['dist-tags'].latest;
+  var downloadLocation = path.resolve(__dirname + '/modules/'
+                                      + name + '/' + version);
+  var destination = path.resolve(__dirname + '/standalone/'
+                                 + name + '/' + version);
+  console.log('fetch: ' + name + '@' + version);
+  return fetchNpm(name, version, downloadLocation)
+  .then(null, onFail('fetch-fail: ' + name + '@' + version))
+  .then(function (pkg) {
+    var downloads = pkg.downloads || [];
+    var main = typeof pkg.browser === 'string' ? pkg.browser : (pkg.main || 'index.js');
+    if (!downloads.some(function (d) { return d === main || d.file === main; })) {
+      return stat(path.join(downloadLocation, main)).then(function (stat) {
+        if (stat.isFile()) {
+          downloads.push({
+            file: main,
+            name: name,
+            description: 'The main code for ' + pkg.name
+          });
+        }
+        return downloads;
+      }, function (err) {
+        console.dir(err.message);
+        return downloads;
+      });
+    } else {
+      return downloads;
+    }
+  }).then(function (downloads) {
+    downloads = downloads.map(function (d) {
+      if (typeof d === 'string') {
+        return {file: d, name: name};
+      } else {
+        return d;
+      }
+    });
+    downloads.forEach(function (d) {
+      d.file = d.file.replace(/\\/g, '/').replace(/^\.?\//, '')
+    });
+    if (downloads.length) {
+      console.log('build: ' + name + '@' + version);
+      console.dir(downloads);
+      return build(downloads, downloadLocation, destination)
+      .then(null, onFail('build-fail: ' + name + '@' + version));
+    }
+  });
+}
+function onFail(message) {
+  return function (err) {
+    console.log(message);
+    throw err;
+  }
+}
 
 
 
